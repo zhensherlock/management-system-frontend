52ecad1ca23c7fd6b6c71a2eb05b90c2f50d06a4
MichaelSun <zhensherlock@126.com>
2023-07-06 17:04:50 +0800

    feat: initial framework

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import NProgress from 'nprogress' // progress bar
 import HomeView from '../views/HomeView.vue'
+import createRouteGuard from './guard'
+
+NProgress.configure({ showSpinner: false }); // NProgress Configuration
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -25,7 +29,12 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('@/views/AboutView.vue')
     }
-  ]
+  ],
+  scrollBehavior() {
+    return { top: 0 };
+  },
 })
+
+createRouteGuard(router);
 
 export default router
