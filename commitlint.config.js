8bc56785eba87eda28bc8ce29a8ecd6fd37e2fe2
MichaelSun <zhensherlock@126.com>
2023-07-12 12:27:52 +0800

    ci: update framework

--- commitlint.config.js
+++ commitlint.config.js
@@ -5,7 +5,23 @@ module.exports = {
     'type-enum': [
       2,
       'always',
-      ['build', 'chore', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test', 'types'],
+      [
+        'feat', // 新功能
+        'fix', //  修复
+        'docs', // 文档变更
+        'style', // 代码格式
+        'refactor', // 重构
+        'perf', // 性能优化
+        'test', // 增加测试
+        'build', // 构建
+        'ci', // CI配置
+        'chore', // 构建过程或辅助工具的变动
+        'revert', // 回退
+        'build', // 打包
+        'types',
+      ],
     ],
+    // subject 大小写不做校验
+    'subject-case': [0],
   },
 };
